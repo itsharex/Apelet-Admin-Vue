8e89fb4506d5c0a5d7503aa729a08137815be7fd
xiaoyuan-zs <2941660384@qq.com>
2023-12-14 21:23:26 +0800

    fix: 完善登录页面粒子效果

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,35 @@
 <template>
     <div class="login-container">
-        <Particles id="ts-particles" class="login-particles" :options="options" />
+        <vue-particles
+            id="tsparticles"
+            :particles-init="particlesInit"
+            :particles-loaded="particlesLoaded"
+            :options="options"
+        />
+        <top-nav />
     </div>
 </template>
 
 <script setup lang="ts" name="Login">
+import TopNav from './components/TopNav.vue';
+// 粒子动画
+import { loadSlim } from 'tsparticles-slim';
 import { particles } from './helpers/particlesConfig';
 
-const options = toRefs(particles);
+const { options } = toRefs(particles);
+
+const particlesInit = async engine => {
+    await loadSlim(engine);
+};
+
+const particlesLoaded = async container => {
+    console.log('Particles container loaded', container);
+};
 </script>
 
 <style scoped lang="scss">
 .login-container {
-    @apply flex-center;
-}
-.login-particles {
-    @apply relative wh-full bg-cover dark bg-no-repeat bg-center-top;
+    @apply relative wh-full bg-cover bg-no-repeat bg-center-top;
 
     background-image: url('@/assets/images/background.svg');
 }
