e8fe56172dce462108517768d78414cb32e76247
xiaoyuan-zs-desk <lcshuxiaoluo@163.com>
2024-02-29 22:38:34 +0800

    fix: 修复i18n错误问题

--- src/permission.ts
+++ src/permission.ts
@@ -1,10 +1,10 @@
 import router from '@/router';
+import i18n from '@/i18n';
 import { NavigationGuardNext, RouteLocationNormalized, RouteRecordRaw } from 'vue-router';
 import { start, done } from '@/config/nprogress';
 // 单独导入，如果小仓库在大仓库外面，无法获取，需要有大仓库
 import { useUserStore, usePermissionStore } from '@/store';
 import { settingConfig } from '@/config/settings';
-import { useI18n } from 'vue-i18n';
 
 // 白名单
 const whiteList = ['/login'];
@@ -15,11 +15,10 @@ router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormali
     // pinia搭配路由使用时，必须放在守卫里才行，不然外面pinia先加载会失效
     const userStore = useUserStore();
     const permissionStore = usePermissionStore();
-    const { t } = useI18n();
     const { title } = settingConfig;
 
     if (to.meta.title) {
-        document.title = t(to.meta.title) + '-' + title;
+        document.title = i18n.global.t(to.meta.title) + '-' + title;
     }
     start();
     if (userStore.token) {
