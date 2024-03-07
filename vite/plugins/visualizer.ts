6eb201302eed2e3b704821304fd638f7d95a6cb1
YY <2941660384@qq.com>
2024-03-07 23:35:23 +0800

    fix: 修改

--- vite/plugins/visualizer.ts
+++ vite/plugins/visualizer.ts
@@ -1,6 +1,6 @@
 import { visualizer } from 'rollup-plugin-visualizer';
 
-export default function createVisualizer(env?: Record<string, string>) {
+export default function createVisualizer() {
     return visualizer({
         open: true,
         filename: 'visualizer.html' //分析图生成的文件名
