2b8305117bdfe47045a34e44bf99a157d6ed394c
YY <2941660384@qq.com>
2024-03-07 22:13:44 +0800

    build: 修改分包优化

--- vite.config.ts
+++ vite.config.ts
@@ -63,13 +63,12 @@ export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
                 output: {
                     chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
                     entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
-                    assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
-
-                    // js最小拆分包
-                    manualChunks(id) {
-                        if (id.includes('node_modules')) {
-                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                        }
+                    assetFileNames: '[ext]/[name]-[hash].[ext]' // 资源文件像 字体，图片等
+                },
+                // js分包优化
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
                     }
                 }
             }
