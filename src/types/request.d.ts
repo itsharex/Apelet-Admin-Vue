2e551d9938386b8527f8abc40f57e7fa329c808b
yuandh@hzcacs.com <yuandh@hzcacs.com>
2024-03-19 15:30:59 +0800

    style: 修改请求类型

---
+++ src/types/request.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="vite/client" />
+
+import { AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
+
+declare global {
+    // 自定义请求接口headers头参数类型
+    type RequestHeader = AxiosRequestHeaders & { token?: string };
+
+    // 自定义请求接口request参数类型，可以加一些自己自定义的参数
+    interface RequestConfig extends AxiosRequestConfig {
+        // 放入请求头
+        headers?: RequestHeader;
+    }
+    // 请求拦截器类型
+    interface RequestInterceptorsConfig extends RequestConfig {
+        // 请求拦截器使用
+        headers: RequestHeader;
+    }
+
+    // 传入泛型约束返回数据类型
+    // ApiResponse 主体后端返回格式
+    interface ApiResponse<T = any> {
+        code: number;
+        msg: string;
+        data: T; // 这里定义请求返回data数据类型
+    }
+}
