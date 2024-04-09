d91846a785311ff7fe1f1dfdfd6b30620217315f
yuandh@hzcacs.com <yuandh@hzcacs.com>
2024-04-09 16:09:13 +0800

    style: mitt 增加类型

--- src/utils/mittBus.ts
+++ src/utils/mittBus.ts
@@ -1,2 +1,8 @@
-import mitt from 'mitt';
-export default mitt();
+import mitt, { type Emitter } from 'mitt';
+
+/** 全局公共事件需要在此处添加类型 */
+type Events = {
+    recoverMenuEvent: string;
+};
+const emit: Emitter<Events> = mitt<Events>();
+export default emit;
