06c50836f585ef30896b9a469a2ff559bcfb0302
xiaoyuan-zs-desk <lcshuxiaoluo@163.com>
2024-04-09 23:45:17 +0800

    feat: 增加vxe国际化

--- src/i18n/locales.ts
+++ src/i18n/locales.ts
@@ -1,9 +1,17 @@
 import zhCN from './zh-cn';
 import en from './en';
+import vxeZhCN from 'vxe-table/lib/locale/lang/zh-CN';
+import vxeEnUS from 'vxe-table/lib/locale/lang/en-US';
 
 const locales = {
-    'zh-CN': zhCN,
-    en: en
+    'zh-CN': {
+        ...zhCN,
+        ...vxeZhCN
+    },
+    en: {
+        ...en,
+        ...vxeEnUS
+    }
 };
 
 export type LocaleKey = keyof typeof locales;
